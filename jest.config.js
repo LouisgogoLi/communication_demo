f2f013eba78efc186b3d678f7a21f57c6f518e53
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2022-06-01 14:42:20 +0800

    更新vue3-jest版本 新增jest.init.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   collectCoverage: true,
   preset: "@vue/cli-plugin-unit-jest",
+  setupFiles: ["./tests/unit/jest.init.js"],
+  reporters: [
+    "default",
+    [
+      "jest-html-reporters",
+      {
+        publicPath: "report/jest-html-reporters",
+      },
+    ],
+  ],
 };
